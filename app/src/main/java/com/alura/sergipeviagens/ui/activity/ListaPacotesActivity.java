44863430dd09c466dfa7af89a60fe72ee43f4f68
Fabiano-Franca <fabianofranca.ti@gmail.com>
2020-07-30 16:13:36 -0300

    Initial commit

---
+++ app/src/main/java/com/alura/sergipeviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package com.alura.sergipeviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alura.sergipeviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
